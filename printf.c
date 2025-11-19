2f987dd241af57ec66bb9566c52f61e84f879fad
“bakosh2” <bakosh2@hotmail.com>
2025-11-19 16:22:57 +0000

    write the code for printf.c file

---
+++ printf.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdarg.h>
+/**
+ * _printf - produces output according to a format
+ * @format: format string containing format specifiers
+ * Return: number of characters printed (excluding null byte)
+*/
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '\0')
+				return (-1);
+			if (*format == 'c')
+				count += _putchar(va_arg(args, int));
+			else if (*format == 's')
+				count += print_string(va_arg(args, char *));
+			else if (*format == '%')
+				count += _putchar('%');
+			else if (*format == 'd' || *format == 'i')
+				count += print_number(va_arg(args, int));
+			else
+			{
+				count += _putchar('%');
+				count += _putchar(*format);
+			}
+		}
+		else
+		{
+			count += _putchar(*format);
+		}
+		format++;
+	}
+
+	va_end(args);
+	return (count);
+}
