a5b71c6be8896536164b7ee457feb4b449c15b27
Fai-Web-Lab <fai.alsharekh@gmail.com>
2025-11-21 15:55:55 +0000

    adding the code for the 1st advanced task

--- functions.c
+++ functions.c
@@ -58,3 +58,36 @@ int print_number(int n)
 
 	return (count);
 }
+
+/**
+ * print_binary - prints an unsigned int in binary
+ * @n: unsigned int number
+ *
+ * Return: number of characters printed
+*/
+int print_binary(unsigned int n)
+{
+	int count = 0;
+	unsigned int mask = 1 << (sizeof(unsigned int) * 8 - 1);
+	int started = 0;
+
+	if (n == 0)
+		return (_putchar('0'));
+
+	while (mask)
+	{
+		if (n & mask)
+		{
+			_putchar('1')
+				count++;
+			started = 1;
+		}
+		else if (started)
+		{
+			_putchar('0');
+			count++;
+		}
+		mask >>= 1;
+	}
+	return (count);
+}
--- printf.c
+++ printf.c
@@ -30,6 +30,8 @@ int _printf(const char *format, ...)
 				count += _putchar('%');
 			else if (*format == 'd' || *format == 'i')
 				count += print_number(va_arg(args, int));
+			else if (*format == 'b')
+				count += print_binary(va_arg(args, unsigned int));
 			else
 			{
 				count += _putchar('%');
